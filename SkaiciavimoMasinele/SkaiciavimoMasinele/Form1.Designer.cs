e24877db5d1fe9707c2cf2000b2b5e86b3b8a6b8
Tomas <karciauskas.tomas@gmail.com>
2020-03-09 10:02:26 +0200

    Initial commit

---
+++ SkaiciavimoMasinele/SkaiciavimoMasinele/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace SkaiciavimoMasinele
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainTxtBx = new System.Windows.Forms.TextBox();
+            this.septyniBtn = new System.Windows.Forms.Button();
+            this.astuoniBtn = new System.Windows.Forms.Button();
+            this.devyniBtn = new System.Windows.Forms.Button();
+            this.daugybaBtn = new System.Windows.Forms.Button();
+            this.minusBtn = new System.Windows.Forms.Button();
+            this.pliusBtn = new System.Windows.Forms.Button();
+            this.lyguBtn = new System.Windows.Forms.Button();
+            this.kablelisBtn = new System.Windows.Forms.Button();
+            this.trysBtn = new System.Windows.Forms.Button();
+            this.sesiBtn = new System.Windows.Forms.Button();
+            this.penkiBtn = new System.Windows.Forms.Button();
+            this.duBtn = new System.Windows.Forms.Button();
+            this.nulisBtn = new System.Windows.Forms.Button();
+            this.vienasBtn = new System.Windows.Forms.Button();
+            this.keturiBtn = new System.Windows.Forms.Button();
+            this.trintiBtn = new System.Windows.Forms.Button();
+            this.dalybaBtn = new System.Windows.Forms.Button();
+            this.mintisTxtBx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // mainTxtBx
+            // 
+            this.mainTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.mainTxtBx.Location = new System.Drawing.Point(12, 55);
+            this.mainTxtBx.Name = "mainTxtBx";
+            this.mainTxtBx.ReadOnly = true;
+            this.mainTxtBx.Size = new System.Drawing.Size(150, 38);
+            this.mainTxtBx.TabIndex = 0;
+            this.mainTxtBx.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // septyniBtn
+            // 
+            this.septyniBtn.Location = new System.Drawing.Point(12, 99);
+            this.septyniBtn.Name = "septyniBtn";
+            this.septyniBtn.Size = new System.Drawing.Size(33, 30);
+            this.septyniBtn.TabIndex = 1;
+            this.septyniBtn.Text = "7";
+            this.septyniBtn.UseVisualStyleBackColor = true;
+            this.septyniBtn.Click += new System.EventHandler(this.septyniBtn_Click);
+            // 
+            // astuoniBtn
+            // 
+            this.astuoniBtn.Location = new System.Drawing.Point(51, 99);
+            this.astuoniBtn.Name = "astuoniBtn";
+            this.astuoniBtn.Size = new System.Drawing.Size(33, 30);
+            this.astuoniBtn.TabIndex = 1;
+            this.astuoniBtn.Text = "8";
+            this.astuoniBtn.UseVisualStyleBackColor = true;
+            this.astuoniBtn.Click += new System.EventHandler(this.astuoniBtn_Click);
+            // 
+            // devyniBtn
+            // 
+            this.devyniBtn.Location = new System.Drawing.Point(90, 99);
+            this.devyniBtn.Name = "devyniBtn";
+            this.devyniBtn.Size = new System.Drawing.Size(33, 30);
+            this.devyniBtn.TabIndex = 1;
+            this.devyniBtn.Text = "9";
+            this.devyniBtn.UseVisualStyleBackColor = true;
+            this.devyniBtn.Click += new System.EventHandler(this.devyniBtn_Click);
+            // 
+            // daugybaBtn
+            // 
+            this.daugybaBtn.Location = new System.Drawing.Point(129, 135);
+            this.daugybaBtn.Name = "daugybaBtn";
+            this.daugybaBtn.Size = new System.Drawing.Size(33, 30);
+            this.daugybaBtn.TabIndex = 1;
+            this.daugybaBtn.Text = "*";
+            this.daugybaBtn.UseVisualStyleBackColor = true;
+            this.daugybaBtn.Click += new System.EventHandler(this.daugybaBtn_Click);
+            // 
+            // minusBtn
+            // 
+            this.minusBtn.Location = new System.Drawing.Point(129, 171);
+            this.minusBtn.Name = "minusBtn";
+            this.minusBtn.Size = new System.Drawing.Size(33, 30);
+            this.minusBtn.TabIndex = 1;
+            this.minusBtn.Text = "-";
+            this.minusBtn.UseVisualStyleBackColor = true;
+            this.minusBtn.Click += new System.EventHandler(this.minusBtn_Click);
+            // 
+            // pliusBtn
+            // 
+            this.pliusBtn.Location = new System.Drawing.Point(129, 207);
+            this.pliusBtn.Name = "pliusBtn";
+            this.pliusBtn.Size = new System.Drawing.Size(33, 30);
+            this.pliusBtn.TabIndex = 1;
+            this.pliusBtn.Text = "+";
+            this.pliusBtn.UseVisualStyleBackColor = true;
+            this.pliusBtn.Click += new System.EventHandler(this.pliusBtn_Click);
+            // 
+            // lyguBtn
+            // 
+            this.lyguBtn.Location = new System.Drawing.Point(90, 243);
+            this.lyguBtn.Name = "lyguBtn";
+            this.lyguBtn.Size = new System.Drawing.Size(72, 30);
+            this.lyguBtn.TabIndex = 1;
+            this.lyguBtn.Text = "=";
+            this.lyguBtn.UseVisualStyleBackColor = true;
+            this.lyguBtn.Click += new System.EventHandler(this.lyguBtn_Click);
+            // 
+            // kablelisBtn
+            // 
+            this.kablelisBtn.Location = new System.Drawing.Point(90, 207);
+            this.kablelisBtn.Name = "kablelisBtn";
+            this.kablelisBtn.Size = new System.Drawing.Size(33, 30);
+            this.kablelisBtn.TabIndex = 1;
+            this.kablelisBtn.Text = ",";
+            this.kablelisBtn.UseVisualStyleBackColor = true;
+            this.kablelisBtn.Click += new System.EventHandler(this.kablelisBtn_Click);
+            // 
+            // trysBtn
+            // 
+            this.trysBtn.Location = new System.Drawing.Point(90, 171);
+            this.trysBtn.Name = "trysBtn";
+            this.trysBtn.Size = new System.Drawing.Size(33, 30);
+            this.trysBtn.TabIndex = 1;
+            this.trysBtn.Text = "3";
+            this.trysBtn.UseVisualStyleBackColor = true;
+            this.trysBtn.Click += new System.EventHandler(this.trysBtn_Click);
+            // 
+            // sesiBtn
+            // 
+            this.sesiBtn.Location = new System.Drawing.Point(90, 135);
+            this.sesiBtn.Name = "sesiBtn";
+            this.sesiBtn.Size = new System.Drawing.Size(33, 30);
+            this.sesiBtn.TabIndex = 1;
+            this.sesiBtn.Text = "6";
+            this.sesiBtn.UseVisualStyleBackColor = true;
+            this.sesiBtn.Click += new System.EventHandler(this.sesiBtn_Click);
+            // 
+            // penkiBtn
+            // 
+            this.penkiBtn.Location = new System.Drawing.Point(51, 135);
+            this.penkiBtn.Name = "penkiBtn";
+            this.penkiBtn.Size = new System.Drawing.Size(33, 30);
+            this.penkiBtn.TabIndex = 1;
+            this.penkiBtn.Text = "5";
+            this.penkiBtn.UseVisualStyleBackColor = true;
+            this.penkiBtn.Click += new System.EventHandler(this.penkiBtn_Click);
+            // 
+            // duBtn
+            // 
+            this.duBtn.Location = new System.Drawing.Point(51, 171);
+            this.duBtn.Name = "duBtn";
+            this.duBtn.Size = new System.Drawing.Size(33, 30);
+            this.duBtn.TabIndex = 1;
+            this.duBtn.Text = "2";
+            this.duBtn.UseVisualStyleBackColor = true;
+            this.duBtn.Click += new System.EventHandler(this.duBtn_Click);
+            // 
+            // nulisBtn
+            // 
+            this.nulisBtn.Location = new System.Drawing.Point(12, 207);
+            this.nulisBtn.Name = "nulisBtn";
+            this.nulisBtn.Size = new System.Drawing.Size(72, 30);
+            this.nulisBtn.TabIndex = 1;
+            this.nulisBtn.Text = "0";
+            this.nulisBtn.UseVisualStyleBackColor = true;
+            this.nulisBtn.Click += new System.EventHandler(this.nulisBtn_Click);
+            // 
+            // vienasBtn
+            // 
+            this.vienasBtn.Location = new System.Drawing.Point(12, 171);
+            this.vienasBtn.Name = "vienasBtn";
+            this.vienasBtn.Size = new System.Drawing.Size(33, 30);
+            this.vienasBtn.TabIndex = 1;
+            this.vienasBtn.Text = "1";
+            this.vienasBtn.UseVisualStyleBackColor = true;
+            this.vienasBtn.Click += new System.EventHandler(this.vienasBtn_Click);
+            // 
+            // keturiBtn
+            // 
+            this.keturiBtn.Location = new System.Drawing.Point(12, 135);
+            this.keturiBtn.Name = "keturiBtn";
+            this.keturiBtn.Size = new System.Drawing.Size(33, 30);
+            this.keturiBtn.TabIndex = 1;
+            this.keturiBtn.Text = "4";
+            this.keturiBtn.UseVisualStyleBackColor = true;
+            this.keturiBtn.Click += new System.EventHandler(this.keturiBtn_Click);
+            // 
+            // trintiBtn
+            // 
+            this.trintiBtn.Location = new System.Drawing.Point(12, 243);
+            this.trintiBtn.Name = "trintiBtn";
+            this.trintiBtn.Size = new System.Drawing.Size(72, 30);
+            this.trintiBtn.TabIndex = 1;
+            this.trintiBtn.Text = "C";
+            this.trintiBtn.UseVisualStyleBackColor = true;
+            this.trintiBtn.Click += new System.EventHandler(this.trintiBtn_Click);
+            // 
+            // dalybaBtn
+            // 
+            this.dalybaBtn.Location = new System.Drawing.Point(129, 99);
+            this.dalybaBtn.Name = "dalybaBtn";
+            this.dalybaBtn.Size = new System.Drawing.Size(33, 30);
+            this.dalybaBtn.TabIndex = 1;
+            this.dalybaBtn.Text = "/";
+            this.dalybaBtn.UseVisualStyleBackColor = true;
+            this.dalybaBtn.Click += new System.EventHandler(this.dalybaBtn_Click);
+            // 
+            // mintisTxtBx
+            // 
+            this.mintisTxtBx.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.mintisTxtBx.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.mintisTxtBx.Location = new System.Drawing.Point(12, 12);
+            this.mintisTxtBx.Name = "mintisTxtBx";
+            this.mintisTxtBx.ReadOnly = true;
+            this.mintisTxtBx.Size = new System.Drawing.Size(150, 31);
+            this.mintisTxtBx.TabIndex = 0;
+            this.mintisTxtBx.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(184, 329);
+            this.Controls.Add(this.keturiBtn);
+            this.Controls.Add(this.sesiBtn);
+            this.Controls.Add(this.vienasBtn);
+            this.Controls.Add(this.trysBtn);
+            this.Controls.Add(this.nulisBtn);
+            this.Controls.Add(this.kablelisBtn);
+            this.Controls.Add(this.duBtn);
+            this.Controls.Add(this.lyguBtn);
+            this.Controls.Add(this.penkiBtn);
+            this.Controls.Add(this.pliusBtn);
+            this.Controls.Add(this.minusBtn);
+            this.Controls.Add(this.dalybaBtn);
+            this.Controls.Add(this.daugybaBtn);
+            this.Controls.Add(this.devyniBtn);
+            this.Controls.Add(this.astuoniBtn);
+            this.Controls.Add(this.trintiBtn);
+            this.Controls.Add(this.septyniBtn);
+            this.Controls.Add(this.mintisTxtBx);
+            this.Controls.Add(this.mainTxtBx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mainTxtBx;
+        private System.Windows.Forms.Button septyniBtn;
+        private System.Windows.Forms.Button astuoniBtn;
+        private System.Windows.Forms.Button devyniBtn;
+        private System.Windows.Forms.Button daugybaBtn;
+        private System.Windows.Forms.Button minusBtn;
+        private System.Windows.Forms.Button pliusBtn;
+        private System.Windows.Forms.Button lyguBtn;
+        private System.Windows.Forms.Button kablelisBtn;
+        private System.Windows.Forms.Button trysBtn;
+        private System.Windows.Forms.Button sesiBtn;
+        private System.Windows.Forms.Button penkiBtn;
+        private System.Windows.Forms.Button duBtn;
+        private System.Windows.Forms.Button nulisBtn;
+        private System.Windows.Forms.Button vienasBtn;
+        private System.Windows.Forms.Button keturiBtn;
+        private System.Windows.Forms.Button trintiBtn;
+        private System.Windows.Forms.Button dalybaBtn;
+        private System.Windows.Forms.TextBox mintisTxtBx;
+    }
+}
+
---
+++ SkaiciavimoMasinele/SkaiciavimoMasinele/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkaiciavimoMasinele
+{
+    public partial class Form1 : Form
+    {
+        private double atmintiesSk;   
+        public Form1()
+        {
+            InitializeComponent();
+            atmintiesSk = 0;
+        }
+
+        
+
+        private void vienasBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Text += '1';
+        }
+
+        private void duBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Text += '2';
+        }
+
+        private void trysBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Text += '3';
+        }
+
+        private void keturiBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Text += '4';
+        }
+
+        private void penkiBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Text += '5';
+        }
+
+        private void sesiBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Text += '6';
+        }
+        private void septyniBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Text += '7';
+        }
+
+        private void astuoniBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Text += '8';
+        }
+
+        private void devyniBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Text += '9';
+        }
+
+        private void pliusBtn_Click(object sender, EventArgs e)
+        {
+            if (mainTxtBx.Text != "")
+            {
+                atmintiesSk += Convert.ToDouble(mainTxtBx.Text);
+                mintisTxtBx.Text = atmintiesSk + " + ";
+                mainTxtBx.Clear();
+            }
+            
+        }
+
+        private void minusBtn_Click(object sender, EventArgs e)
+        {
+            if (mainTxtBx.Text != "")
+            {
+                atmintiesSk -= Convert.ToDouble(mainTxtBx.Text);
+                mintisTxtBx.Text = atmintiesSk + " - ";
+                mainTxtBx.Clear();
+            }
+        }
+
+        private void daugybaBtn_Click(object sender, EventArgs e)
+        {
+            if (mainTxtBx.Text != "")
+            {
+                if (atmintiesSk == 0)
+                {
+                    atmintiesSk = 1;
+                }
+                atmintiesSk *= Convert.ToDouble(mainTxtBx.Text);
+                mintisTxtBx.Text = atmintiesSk + " * ";
+
+                mainTxtBx.Clear();
+            }
+        }
+
+        private void dalybaBtn_Click(object sender, EventArgs e)
+        {
+            if (mainTxtBx.Text != "")
+            {
+                if (atmintiesSk == 0)
+                {
+                    atmintiesSk = 1;
+                }
+                atmintiesSk /= Convert.ToDouble(mainTxtBx.Text);
+                mintisTxtBx.Text = atmintiesSk + " / ";
+
+                mainTxtBx.Clear();
+            }
+        }
+
+        private void lyguBtn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void trintiBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Clear();
+            mintisTxtBx.Text = "";
+        }
+
+        private void kablelisBtn_Click(object sender, EventArgs e)
+        {
+            if (!mainTxtBx.Text.Contains(','))
+            {
+                if (mainTxtBx.Text == "")
+                {
+                    mainTxtBx.Text += '0' + ',';
+                }
+                else
+                {
+                    mainTxtBx.Text += ',';
+                }
+            }
+        }
+
+        private void nulisBtn_Click(object sender, EventArgs e)
+        {
+            mainTxtBx.Text += '0';
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
